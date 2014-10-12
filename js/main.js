904439ef49a6a21038a726ce8007afdef3f8e7ed
Jeffrey Baumes <jeff.baumes@kitware.com>
2014-10-12 12:56:22 -0400

    Initial carousel

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(function () {
+    var data = localStorage.webcarousel ? JSON.parse(localStorage.webcarousel) : undefined,
+        index = 0,
+        $frame = $('.wc-frame'),
+        curTimeout = undefined;
+
+    if (!data || !data.sites || !data.delaySeconds) {
+        data = {
+            delaySeconds: 5,
+            sites: ['http://www.kitware.com']
+        };
+        localStorage.webcarousel = JSON.stringify(data);
+    }
+
+    console.log(JSON.stringify(data, null, 4));
+    $('#settings').text(JSON.stringify(data, null, 4));
+    $('#save').click(function () {
+        data = JSON.parse($('#settings').val());
+        localStorage.webcarousel = $('#settings').val();
+    });
+
+    function showNext() {
+        if (index >= data.sites.length) {
+            index = 0;
+        }
+        console.log(data.sites[index]);
+        $frame.attr('src', data.sites[index]);
+        index += 1;
+        if (curTimeout) {
+            clearTimeout(curTimeout);
+        }
+        curTimeout = setTimeout(showNext, data.delaySeconds * 1000);
+    }
+
+    $('#prev').click(function () {
+        index -= 1;
+        if (index < 0) {
+            index = data.sites.length - 1;
+        }
+        index -= 1;
+        if (index < 0) {
+            index = data.sites.length - 1;
+        }
+        showNext();
+    });
+
+    $('#next').click(function () {
+        showNext();
+    });
+
+    showNext();
+    $('#myModal').modal({show: false});
+    $frame.css('height', $('body').height());
+});
